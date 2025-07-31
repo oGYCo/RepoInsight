598590df83f2a77515d6e74e55a0ee4c21a47a47
oGYCo <mikutianyi@outlook.com>
2025-07-31 19:10:43 +0800

    update

---
+++ __init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+RepoInsight Plugin for LangBot
+
+A LangBot plugin for analyzing GitHub repositories with GithubBot.
+
+Author: RepoInsight Team
+Version: 1.0.0
+"""
+
+# Plugin metadata
+__version__ = "1.0.0"
+__author__ = "RepoInsight Team"
+__description__ = "A LangBot plugin for analyzing GitHub repositories with GithubBot"
+
+# Import main plugin class
+from main import RepoInsightPlugin
+
+__all__ = ['RepoInsightPlugin']
